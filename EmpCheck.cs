e35b39163d11db2c59b43a843625842eb84f41a3
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-09 09:27:45 +0530

    Checking Employee is Present or Absent

---
+++ EmpCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComputationProblem
+{
+    public class EmpCheck
+    {
+        public static void CheckEmployee() {
+            int Is_full_time = 1;
+            Random random= new Random();
+            int empcheck = random.Next(2);
+            if(empcheck == Is_full_time) {
+                Console.WriteLine("Employee is present");
+                    }
+            else
+            {
+                Console.WriteLine("employee is not present");
+            }
+        
+        }
+    }
+}
