ea60b436037f1168b8656a0513c5253397051ba3
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-09 09:12:02 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using EmpWageComputationProblem;
+
+namespace EmpWageComputation
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            EmpCheck.CheckEmployee();
+        }
+    }
+}
