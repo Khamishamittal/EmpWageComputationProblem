d42e2c4b38c817fb2d7d5b5072fa1b6b9af40028
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-10 21:24:00 +0530

    Calculate Emp Wage using switch case

--- EmpCheck.cs
+++ EmpCheck.cs
@@ -5,33 +5,32 @@ namespace EmpWageComputationProblem
     public class EmpCheck
 
     {
-
+       public const int Is_full_time = 1;
+       public const int Emp_Rate_Per_Hour = 20;
+        public const int Is_part_time = 2;
         public static void CheckEmployee()
         {
-            int Is_full_time = 1;
-            int Emp_Rate_Per_Hour = 20;
-            int Is_part_time = 2;
+            
 
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
             int empcheck = random.Next(2);
-
-            if (empcheck == Is_full_time)
+            switch(empcheck)
             {
-                empHrs = 8;
-            }
-            else if (empcheck == Is_part_time)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
-                empWage= 0;
+                case Is_full_time:
+                    empHrs = 8;
+                    break;
+                    case Is_part_time: 
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs= 0; 
+                    break;
 
             }
+            
             empWage = empHrs * Emp_Rate_Per_Hour;
          Console.WriteLine("Emp Wage : " + empWage);
                 }
