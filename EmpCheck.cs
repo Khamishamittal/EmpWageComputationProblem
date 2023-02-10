c9a39ae9bc64ca61866c7f8ec038d29b9d985305
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-10 08:18:05 +0530

    Calculate Total Employee Wage

---
+++ EmpCheck.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace EmpWageComputationProblem
+{
+    public class EmpCheck
+
+    {
+
+        public static void CheckEmployee()
+        {
+            int Is_full_time = 1;
+            int Emp_Rate_Per_Hour = 20;
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empcheck = random.Next(2);
+
+            if (empcheck == Is_full_time)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+
+            }
+            empWage = empHrs * Emp_Rate_Per_Hour;
+         Console.WriteLine("Emp Wage : " + empWage);
+                }
+    }
+}
+
